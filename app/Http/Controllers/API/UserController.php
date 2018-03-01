abbd3601643f454c75f2f7488eb92424b02919ef
Tom Wilson <tom@iwm.pw>
2018-03-01 00:08:25 +0000

    added support for profile photos

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -8,18 +8,31 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Intervention\Image\Facades\Image;
 use Validator;
 
 class UserController extends Controller
 {
-    public $successStatus = 200;
-
     public function DoRegister(Request $request)
     {
         $eighteenYearsAgo = Carbon::now()->subYears(18)->format('Y-m-d');
         
         $messages = array(
-            'before'            => 'You must be aged 18 or over!'
+            'before'                        => 'You must be aged 18 or over!',
+            'full_name.required'            => 'You must enter your full name!',
+            'known_as.required'             => 'You must enter what you want to be known as!',
+            'dob.required'                  => 'You must enter your date of birth!',
+            'dob.date'                      => 'Your date of birth must be in date format!',
+            'postcode.required'             => 'You must enter your postcode!',
+            'postcode.regex'                => 'Your postcode must be in the correct format!',
+            'phone_number.regex'            => 'Your phone number must be in the correct format!',
+            'email.required'                => 'You must enter your email address!',
+            'email.email'                   => 'Your email address must be in the correct format!',
+            'email.users'                   => 'Another user has already registered with that email address!',
+            'password.required'             => 'You must enter a password!',
+            'confirm_password.required'     => 'You must enter the same password in both fields!',
+            'confirm_password.same'         => 'You must enter the same password in both fields!',
+            'profile_photo.size'            => 'Your profile photo cannot be larger than 1GB!'
         );
 
         $validator = Validator::make($request->all(),
@@ -39,15 +52,23 @@ class UserController extends Controller
             return response()->json(['error' => $validator->errors()], 200);            
         }
 
-        $input              = $request->all();
-        $input['password']  = Hash::make($input['password']);
-        $input['reg_ip']    = $request->ip();
-        $input['dob']        = Carbon::createFromFormat('d-m-Y', $input['dob']);
+        $input                      = $request->all();
+        $input['password']          = Hash::make($input['password']);
+        $input['reg_ip']            = $request->ip();
+        $input['dob']               = Carbon::createFromFormat('d-m-Y', $input['dob']);
 
-        $user               = User::create($input);
-        $success['token']   = $user->createToken('MyApp')->accessToken;
+        if (isset($input['profile_photo']))
+        {
+            $input['profile_photo'] = base64_encode(Image::make($input['profile_photo'])->fit(500, 500, function ($constraint)
+                                                                                        {
+                                                                                            $constraint->upsize();
+                                                                                        })->encode()->encoded);
+        }
 
-        return response()->json(['success' => $success], $this->successStatus);
+        $user                       = User::create($input);
+        $success['token']           = $user->createToken('MyApp')->accessToken;
+
+        return response()->json(['success' => $success], 200);
     }
 
     public function DoLogin()
@@ -56,7 +77,7 @@ class UserController extends Controller
         {
             $user = Auth::user();
             $success['token'] = $user->createToken('MyApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
+            return response()->json(['success' => $success], 200);
         }
         else
         {
@@ -67,6 +88,6 @@ class UserController extends Controller
     public function GetDetails()
     {
         $user = Auth::user();
-        return response()->json(['success' => $user], $this->successStatus);
+        return response()->json(['success' => $user], 200);
     }
 }
